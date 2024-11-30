0e68851f070a3abc268e76d4d30ab890c73962cf
BeatrizBueno08 <buenodecarvalhobeatriz@gmail.com>
2024-11-30 16:27:52 -0300

    upando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Educação Financeira',
+    'Por que é importante ter uma reserva financeira?',
+    'Ter uma reserva financeira garante segurança em emergências, reduz o estresse, evita endividamento, oferece liberdade e flexibilidade, além de permitir melhor planejamento e aproveitar oportunidades sem comprometer as finanças.'
+)
+
+criaCartao(
+    'Saúde e Bem-estar',
+    'Por que é importante praticar exercícios físicos?',
+    'Praticar exercícios físicos melhora a saúde cardiovascular, controla o peso, fortalece músculos e ossos, aumenta a energia, reduz o estresse e ansiedade, e promove longevidade, melhorando a qualidade de vida e prevenindo doenças.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é um número primo?',
+    'Um número primo é um número natural maior que 1 que só pode ser dividido por 1 e por ele mesmo.'
+)
+
+criaCartao(
+    'Valores Humanos',
+    'Quais são os valores mais importantes na vida?',
+    'Os valores mais importantes podem variar de pessoa para pessoa, mas incluem a honestidade, o respeito, a compaixão, a liberdade e a responsabilidade.'
+)
